67aff60b8b5cf7a744733ebf624085d9c3d2e68d
qiujietian <tqj.zyy@gmail.com>
2025-04-03 10:12:04 +0800

    fix audio9

--- flutter/flutter/ios/Classes/FFmpegKitFlutterPlugin.m
+++ flutter/flutter/ios/Classes/FFmpegKitFlutterPlugin.m
@@ -19,12 +19,7 @@
 
 #import "FFmpegKitFlutterPlugin.h"
 
-// 尝试这些导入方式（一次只使用一种）
 #import <ffmpegkit/FFmpegKitConfig.h>
-// 或
-#import "ffmpegkit.h"
-// 或
-#import <ffmpegkit.h>
 
 static NSString *const PLATFORM_NAME = @"ios";
 
